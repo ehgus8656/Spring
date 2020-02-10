bb6fa49b95780dc5312335f15261ecf2280e78a1
ehgus8656 <ehak159@gmail.com>
2020-02-10 14:52:09 +0900

    Spring01

---
+++ src/main/java/spring/Main.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		
+		Greeter g = ctx.getBean("greeter",Greeter.class);
+		String msg = g.greet("Spring!");
+		System.out.println(msg);
+		ctx.close();
+	}
+}
